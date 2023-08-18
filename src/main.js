2094e726ca35196814710ff9fee7b95fec637949
Ariadna Frias <ariadna.812@gmail.com>
2023-08-18 17:30:38 +0200

    Added push method

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
-// LENGTH
 const array1 = ["1", "2", "3", "hello"];
+
+// LENGTH
 const arrayLength = (array) => {
   let count = 0;
   array.forEach((index) => count++);
   return count;
 };
-console.log(arrayLength(array1));
+//console.log(arrayLength(array1));
 
 // PUSH
+const arrayPush = (array, newValue) => {
+  let n = arrayLength(array);
+  array[n] = newValue;
+  return n + 1;
+};
+console.log(arrayPush(array1, "bye"));
+console.log(array1);
