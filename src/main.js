90d62a1230a9b3c027acdf21aa921c8f6e44b92f
Ariadna Frias <ariadna.812@gmail.com>
2023-08-18 12:11:03 +0200

    Added length method

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 // LENGTH
+const array1 = ["1", "2", "3", "hello"];
+const arrayLength = (array) => {
+  let count = 0;
+  array.forEach((index) => count++);
+  return count;
+};
+console.log(arrayLength(array1));
+
+// PUSH
