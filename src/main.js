47893769e95b0ca42e170fe97e0c85e6f0b7004c
Ariadna Frias <ariadna.812@gmail.com>
2023-08-20 17:57:59 +0200

    Added unshift method

--- src/main.js
+++ src/main.js
@@ -14,5 +14,50 @@ const arrayPush = (array, newValue) => {
   array[n] = newValue;
   return n + 1;
 };
-console.log(arrayPush(array1, "bye"));
-console.log(array1);
+//console.log(arrayPush(array1, "bye"));
+//console.log(array1);
+
+// POP
+const arrayPop = (array) => {
+  let n = arrayLength(array);
+  let d = array[n - 1];
+  array.length = n - 1;
+  return d;
+};
+//console.log(arrayPop(array1));
+//console.log(array1);
+
+// UNSHIFT
+const arrayUnshift = (array, newValue) => {
+  let n = arrayLength(array);
+  do {
+    array[n] = array[n - 1];
+    n--;
+  } while (n > 0);
+  array[0] = newValue;
+  return arrayLength(array);
+};
+//console.log(arrayUnshift(array1, "foo"));
+//console.log(array1);
+
+// SHIFT
+
+// SOME
+
+// EVERY
+
+// FIND
+
+// FILTER
+
+// MAP
+
+// FINDINDEX
+
+// INCLUDES
+
+// INDEXOF
+
+// REDUCE
+
+// JOIN
